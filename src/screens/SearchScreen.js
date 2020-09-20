c6beb8ee0fa0781234fd222d95c4d4ec7b8051d4
Harshit B <harshitbhatt972@gmail.com>
2020-09-20 23:51:51 +0530

    Error handling is completed for api in home page

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,10 +5,11 @@ import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
+        try{
         const response = await yelp.get('/search',{
             params: {
                 limit: 50,
@@ -17,7 +18,10 @@ const SearchScreen = () => {
             }
         });
         setResults(response.data.businesses)
+    } catch (err) {
+        setErrorMessage("Somthing went wrong");
     }
+    };
 
     return (
     <View>
@@ -26,7 +30,7 @@ const SearchScreen = () => {
     onTermChange={setTerm} 
     onTermSubmit={searchApi}
     />
-    <Text>Search Screen</Text>
+    {errorMessage ? <Text>{errorMessage}</Text>: null}
     <Text> We have found {results.length} results</Text>
     </View>
     );
