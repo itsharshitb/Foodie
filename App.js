ff6dfada760d15e080231a5f328b783e8d521494
Harshit B <harshitbhatt972@gmail.com>
2020-09-20 19:26:04 +0530

    First commit, added SearchScreen as a home screen

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { registerRootComponent } from 'expo';
+import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',   //first interface after opeaning
+  defaultNavigationOptions: {
+    title: 'BusinessSearch'   //header button
+  }
 });
+
+export default createAppContainer(navigator);   //inbuilt pakage help to show all element inside navigator
