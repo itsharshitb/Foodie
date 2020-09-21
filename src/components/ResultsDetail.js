b38ed5478e10d43491d4f15882429d0ca97dc321
Harshit B <harshitbhatt972@gmail.com>
2020-09-21 21:18:53 +0530

    adding styling

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,22 +3,25 @@ import {View, Text, StyleSheet, Image} from 'react-native';
 
 const ResultsDetail = ({ result }) => {
     return (
-        <View>
+        <View style={styles.container}>
         <Image style={styles.image} source={{uri: result.image_url }}/>
         <Text style={styles.name}>{result.name}</Text>
+        <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container:{
+        marginLeft:10
+    },
     image: {
         width: 250,
         height: 120,
         borderRadius: 4
     },
     name: {
-        fontWeight: 'bold',
-        fontSize: 16
+        fontWeight: 'bold'
     }
 });
 
